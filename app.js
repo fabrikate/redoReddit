d4373f9ec3499ea5715e863452a931b7557c385b
KateStearns <katestearns@mac.com>
2015-09-16 09:59:59 -0700

    initial committ

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express()
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-Override');
+var bodyParser = require('body-Parser');
+var request = require('request');
+var mongoose = require('mongoose');
+var session = require('cookie-session');
+//TO DO: MIDDLEWARE
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+//Set up Cookie Session
+app.use(session({
+  maxAge: 3600000,
+  secret: 'illnevertell',
+  name: 'redditCookie'
+}));
+
