b8d53e4ff8ed846a3602e013e000f3ccbcd4e02b
KateStearns <katestearns@mac.com>
2015-09-17 09:19:39 -0700

    comments now connected

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,27 +1,25 @@
 var db = require('../models/index');
 //index
-app.get('/posts/:post_id/comments', function (req,res){
-  console.log('hey comments');
-  db.Post.findById(req.params.post_id).populate('comments').exec(function (err, post){
+app.get('/posts/:post_id/comments', function (req,res) {
+  db.Post.findById({post: req.params.post_id}).populate('comments').exec(function (err, post) {
     res.render("comments/index", {post: post});
   });
 });
 
 //new
-app.get('/posts/:post_id/comments/new', function (req, res){
-  console.log('hey new')
-  db.Post.findById(req.params.post_id, function (err, post){
+app.get('/posts/:post_id/comments/new', function (req, res) {
+  db.Post.findById({post: req.params.post_id}, function (err, post) {
     res.render('comments/new')
   });
 });
 
 //create
-app.post('/posts/:post_id/comments', function (req,res){
-  db.Comment.create({commentText: req.body.commentText}, function (err, comment){
+app.post('/posts/:post_id/comments', function (req,res) {
+  db.Comment.create({commentText: req.body.commentText}, function (err, comment) {
     if (err){
       res.render('/posts/:post_id/comments/new')
     } else {
-      db.Post.findById(req.params.post_id, function (err, comment){
+      db.Post.findById(req.params.post_id, function (err, comment) {
         user.comments.push(comment);
         user.comment = user.id;
         comment.save();
@@ -33,22 +31,22 @@ app.post('/posts/:post_id/comments', function (req,res){
 });
 
 //show
-app.get('/posts/:post_id/comments/:comment_id', function (req,res){
-  db.Comment.findById(req.params.id).populate('post').exec( function (err, comment){
+app.get('/posts/:post_id/comments/:comment_id', function (req,res) {
+  db.Comment.findById(req.params.id).populate('post').exec( function (err, comment) {
     res.render('comments/show', {comment: comment})
   });
 });
 
 //edit
-app.get('/posts/:post_id/comments/:comment_id/edit', function (req,res){
-  db.Comment.findById(req.params.id).populate('post').exec(function (err, comment){
+app.get('/posts/:post_id/comments/:comment_id/edit', function (req,res) {
+  db.Comment.findById(req.params.id).populate('post').exec(function (err, comment) {
     res.render('comments/edit', {comment: comment})
   });
 });
 
 //update
-app.put('/posts/:post_id/comments/comment_id', function (req,res){
-  db.Comment.findByIdAndUpdate(req.params.id, {commentText: req.body.commentText}, function (err, comment){
+app.put('/posts/:post_id/comments/comment_id', function (req,res) {
+  db.Comment.findByIdAndUpdate(req.params.id, {commentText: req.body.commentText}, function (err, comment) {
     if (err){
       res.render('comments/edit')
     } else {
@@ -57,10 +55,9 @@ app.put('/posts/:post_id/comments/comment_id', function (req,res){
   });
 });
 
-
 //delete
-app.delete('/posts/:post_id/comments', function (req,res){
-  db.Comment.findByIdAndRemove(req.params.id, function(err, comment){
+app.delete('/posts/:post_id/comments', function (req,res) {
+  db.Comment.findByIdAndRemove(req.params.id, function(err, comment) {
     if (err){
       res.render('comments/edit')
     } else {
@@ -68,9 +65,3 @@ app.delete('/posts/:post_id/comments', function (req,res){
     };
   });
 });
-
-
-
-
-
-
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,8 @@ app.get('/', function(req, res) {
   res.redirect('/posts');
 })
 require('./posts');
+require('./comments');
 
-// app.get('*', function(req, res) {
-//   res.render('404');
-// })
+app.get('*', function(req, res) {
+  res.render('404');
+})
